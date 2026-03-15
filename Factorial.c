31ad8a4df4dee1e046415a65330b13bdc03deb67
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-15 15:37:36 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n=5,i;
+    int result=1;
+
+    for(i=1;i<=n;i++){
+        result=result*i;
+    }
+
+    printf("Example program: Factorial\n");
+    printf("Result value: %d",result);
+
+    return 0;
+}
